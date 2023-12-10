16369ddd399d3ac59e3779edb81866ccc24f6360
Bryambepz <bryamparra2709@gmail.com>
2023-12-10 11:56:34 -0500

    Inicio del proyecto backend

---
+++ src/test/java/com/test/cognoware/bryam/parra/testBackend/TestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cognoware.bryam.parra.testBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
